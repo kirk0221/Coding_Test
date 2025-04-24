217b6a2014fd607e10e126ad2b2a62705231e9a8
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-04-25 08:04:47 +0900

    04/24
    04/24

--- BAEKJOON/2447.py
+++ BAEKJOON/2447.py
@@ -1,7 +1,20 @@
-def star(n):
-    if n == 3:
-        return print("***\n* *\n***", end = '')
-    return star(n//3) * (n//3) + print() + star(n//3) + print(' ') * (n//3) + star(n//3) + star(n//3) * (n//3)
+import sys
+sys.setrecursionlimit(10**6)
 
-n = int(input())
-star(n)
+def append_star(LEN):
+    if LEN == 1:
+        return ['*']
+
+    Stars = append_star(LEN//3) 
+    L = []  
+    
+    for S in Stars:
+        L.append(S*3)
+    for S in Stars:
+        L.append(S+' '*(LEN//3)+S)
+    for S in Stars:
+        L.append(S*3)
+    return L
+
+n = int(sys.stdin.readline().strip())
+print('\n'.join(append_star(n)))
